3b99f9a0495c03d7e04cd43a0d7ec303fe0d8385
nishu-murmu <nishumurmu017@gmail.com>
2023-01-03 23:05:14 +0530

    adding additional configs for bufferline

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -1,29 +1,40 @@
-require('bufferline').setup {
-  groups = {
-    options = {
-      toggle_hidden_on_enter = true -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
-    },
-    items = {
+require("bufferline").setup({
+  options = {
+    numbers = "ordinal",
+    offsets = {
       {
-        name = "Tests", -- Mandatory
-        highlight = { gui = "underline", guisp = "blue" }, -- Optional
-        priority = 2, -- determines where it will appear relative to other groups (Optional)
-        icon = "", -- Optional
-        matcher = function(buf) -- Mandatory
-          return buf.name:match('%_test') or buf.name:match('%_spec')
-        end,
+        filetype = "NvimTree",
+        text = "File Explorer",
+        highlight = "Directory",
+        separator = true, -- use a "true" to enable the default, or set your own character
       },
-      {
-        name = "Docs",
-        highlight = { gui = "undercurl", guisp = "green" },
-        auto_close = false, -- whether or not close this group if it doesn't contain the current buffer
-        matcher = function(buf)
-          return buf.name:match('%.md') or buf.name:match('%.txt')
-        end,
-        separator = { -- Optional
-          style = require('bufferline.groups').separator.tab
+    },
+    separator_style = "slant",
+    diagnostics = "nvim_lsp",
+    groups = {
+      options = {
+        toggle_hidden_on_enter = true, -- when you re-enter a hidden group this options re-opens that group so the buffer is visible
+      },
+      items = {
+        require("bufferline.groups").builtin.pinned:with({ icon = "" }),
+        {
+          name = "components", -- Mandatory
+          highlight = { underline = true, sp = "blue" }, -- Optional
+          priority = 2, -- determines where it will appear relative to other groups (Optional)
+          auto_close = true,
+          matcher = function(buf) -- Mandatory
+            return buf.filename:match("%.tsx") or buf.filename:match("%.jsx")
+          end,
         },
-      }
-    }
-  }
-}
+        {
+          name = "Docs",
+          highlight = { undercurl = true, sp = "green" },
+          auto_close = true, -- whether or not close this group if it doesn't contain the current buffer
+          matcher = function(buf)
+            return buf.filename:match("%.md") or buf.filename:match("%.txt")
+          end,
+        },
+      },
+    },
+  },
+})
