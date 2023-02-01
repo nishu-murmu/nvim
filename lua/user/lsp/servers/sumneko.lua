916331f90875dd098005d84c979109f7feb483be
nishu-murmu <nishumurmu017@gmail.com>
2023-02-01 22:32:27 +0530

    fixing nvim-tree error and sumneko lua problem

--- lua/user/lsp/servers/sumneko.lua
+++ lua/user/lsp/servers/sumneko.lua
@@ -1,19 +1,15 @@
 local util = require("lspconfig.util")
 
-local runtime_path = vim.split(package.path, ";")
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
 return {
     settings = {
         Lua = {
-            completion = {
-                callSnippet = "Replace",
-            },
             runtime = {
                 -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
                 version = "LuaJIT",
-                path = runtime_path,
+            },
+            completion = {
+                enable = true,
+                callSnippet = "Both",
             },
             diagnostics = {
                 -- Get the language server to recognize the `vim` global
@@ -21,10 +17,7 @@ return {
             },
             workspace = {
                 library = vim.api.nvim_get_runtime_file("", true),
-                -- library = {
-                --     [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                --     [vim.fn.stdpath("config") .. "/lua"] = true,
-                -- },
+                checkThirdParty = false,
             }, -- Do not send telemetry data containing a randomized but unique identifier
             telemetry = {
                 enable = false,
