6b24ce196ed09b6dd95f7fa20aa938ebc737c6b3
nishu-murmu <nishumurmu017@gmail.com>
2024-07-02 19:20:21 +0530

    fix: updating install script

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-set arch "arch"
 
 # Arch Linux prerequisites
-if [[ $(uname -r) == *"$arch" ]];
-then
+if [[ $(uname -r) == *"arch" ]]; then
+  echo 'gone inside'
   sudo pacman -S git base-devel cmake unzip ninja tree-sitter curl neovim
+else
+  echo "[[ installing cargo: ]]"
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  echo "[[ installing tree-sitter-cli: ]]"
+  cargo install tree-sitter-cli
 fi
 
 cd ../
 mv nvim/ ~/.config/
 
 nvim -c "so $MYVIMRC"
+nvim -c ":q"
 nvim -c "PackerSync"
+nvim -c ":q"
